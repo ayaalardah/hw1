6ba1b303ee11cfb721beaec191e9c4e0f0f38d3f
ayaalardah <154684639+ayaalardah@users.noreply.github.com>
2023-12-23 17:24:03 +0300

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,16 @@
+bookdic={
+'Book_name':'A VISION OF THE HUMAN FUTUER IN SPACE',
+'The writer':'Carl Sagan',
+'Published':'1994',
+'Place of publication':'united states of america',
+'Book type' : 'Non-fiction',
+'Book story':'the book explores the human vision for the futuer in space and provides an analysis of the challenges faced in space exploration and colonization',
+'The topics of the book': [' 1-history of space exploration', '2-astronomy' ,'3-space technology','4-social and cultural implications of space exploration'  ],
+'more information':'the book raises questions and the futuer of humanity and its relationship with the cosmos'
+}
+
+print(bookdic)
+print(len(bookdic))
+print(bookdic.keys())
+print(bookdic['Book_name'])
+print(type(bookdic))
